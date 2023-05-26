32209a6da8b2dc66d70c528b474abaec8ff615d3
Liana Bisuña <liana@thinkbitsolutions.com>
2023-05-26 19:26:31 +0800

    Chore: update page layout

--- src/layouts/LayoutDashboard.tsx
+++ src/layouts/LayoutDashboard.tsx
@@ -1,4 +1,5 @@
 // Imports
+import { AppButton } from "@/components/app";
 import {
 	Bars3Icon,
 	ChevronDownIcon,
@@ -44,26 +45,49 @@ function Header() {
 	const sidebar = sidebarContext ? sidebarContext.sidebar : false;
 
 	return (
-		<header className="flex items-center justify-between h-12 w-full bg-[#111111] px-5">
-			<div>
-				<button className="flex text-sm font-semibold text-white">
-					<span>Button</span>
-					<ChevronDownIcon className="h-5 w-5 text-white ml-1.5" />
-				</button>
-			</div>
-			<div>
-				<button
-					className="bg-transparent outline-none group"
-					onClick={() => setSidebar(!sidebar)}
-				>
-					<Bars3Icon
-						className={`
-							h-5 w-5 text-white transform duration-200
-							${sidebar ? 'rotate-90' : ''}
-						`}
-					/>
-				</button>
-			</div>
+		<header className="flex items-center justify-between h-14 w-full bg-[#111111] px-5">
+			<ul className="flex gap-3 items-center">
+				<li>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant="text"
+					>
+						<CubeIcon className="h-5 w-5" />
+					</AppButton>
+				</li>
+				<li>
+					<AppButton
+						size="xs"
+						color="eerie"
+						tone="dark"
+					>
+						<span>Button</span>
+						<ChevronDownIcon className="h-3 w-3 ml-3 path-stroke-2" />
+					</AppButton>
+				</li>
+			</ul>
+			<ul className="flex gap-3">
+				<li>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant={sidebar?'solid':'text'}
+						onClick={()=>setSidebar(!sidebar)}
+					>
+						<Bars3Icon
+							className={`
+								h-5 w-5 transform duration-200 path-stroke-2
+								${sidebar ? 'rotate-90' : ''}
+							`}
+						/>
+					</AppButton>
+				</li>
+			</ul>
 		</header>
 	);
 }
@@ -92,38 +116,68 @@ function Sidebar() {
 }
 
 function Footer() {
+	const [active, setActive] = useState(false);
+
 	return (
-		<div className="fixed bg-[#111111] rounded-lg h-12 bottom-5 left-1/2 -translate-x-1/2 flex justify-center items-center px-3">
+		<div className="fixed bg-[#111111] rounded-lg h-14 bottom-5 left-1/2 -translate-x-1/2 flex justify-center items-center px-3">
 			<ul className="flex gap-3">
 				<li>
-					<button className="rounded-lg p-1.5">
-						<ChevronLeftIcon className="h-5 w-5 text-white" />
-					</button>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant="text"
+					>
+						<ChevronLeftIcon className="h-5 w-5" />
+					</AppButton>
 				</li>
 				<li>
-					<button className="rounded-lg p-1.5">
-						<CubeIcon className="h-5 w-5 text-white" />
-					</button>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant={active?'solid':'text'}
+						onClick={()=>setActive(!active)}
+					>
+						<DevicePhoneMobileIcon className="h-5 w-5" />
+					</AppButton>
 				</li>
 				<li>
-					<button className="rounded-lg p-1.5">
-						<DevicePhoneMobileIcon className="h-5 w-5 text-white" />
-					</button>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant={active?'solid':'text'}
+						onClick={()=>setActive(!active)}
+					>
+						<ComputerDesktopIcon className="h-5 w-5" />
+					</AppButton>
 				</li>
 				<li>
-					<button className="bg-[#252525] rounded-lg p-1.5">
-						<DeviceTabletIcon className="h-5 w-5 text-white" />
-					</button>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant={active?'solid':'text'}
+						onClick={()=>setActive(!active)}
+					>
+						<DeviceTabletIcon className="h-5 w-5" />
+					</AppButton>
 				</li>
 				<li>
-					<button className="rounded-lg p-1.5">
-						<ComputerDesktopIcon className="h-5 w-5 text-white" />
-					</button>
-				</li>
-				<li>
-					<button className="rounded-lg p-1.5">
-						<ChevronRightIcon className="h-5 w-5 text-white" />
-					</button>
+					<AppButton
+						icon
+						size="xs"
+						color="eerie"
+						tone="dark"
+						variant="text"
+					>
+						<ChevronRightIcon className="h-5 w-5" />
+					</AppButton>
 				</li>
 			</ul>
 		</div>
