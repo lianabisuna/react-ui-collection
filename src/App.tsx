5bef90b0b3f6716f34d029398dee01030204493c
Liana Bisuña <liana@thinkbitsolutions.com>
2023-06-02 20:14:10 +0800

    Chore: adjust screen styles for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
-		<div className="flex h-screen w-full overflow-hidden">
+		<div className="flex h-full w-full overflow-hidden">
 			<Outlet />
 		</div>
   );
