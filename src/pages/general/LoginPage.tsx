394bd275bbfe6df848b5fe1a07fd7e1bdfb76338
Liana Bisuña <liana@thinkbitsolutions.com>
2023-06-12 17:10:37 +0800

    Chore: update landing page

--- src/pages/general/LoginPage.tsx
+++ src/pages/general/LoginPage.tsx
@@ -7,34 +7,33 @@ import { Link } from "react-router-dom";
 export default function LoginPage() {
 	return (
 		<div className="bg-dotted w-full h-full flex justify-center items-center">
-			<div className="h-full w-full max-h-[300px] max-w-[600px] flex flex-col justify-center items-start p-5 md:p-10 gap-8">
-					<div className="w-5/6 md:w-9/12 mx-auto">
-						<p className="font-bold text-white text-3xl">A sandbox of user interface components created for the web</p>
-						<p className="text-xs mt-3 font-medium text-battle">Take a tour of buttons, dropdowns, input fields, and everything visible on the web—made interactive.</p>
+			<div className="h-full w-full max-h-[300px] max-w-[450px] flex flex-col justify-center items-center p-5 md:p-10 gap-8 bg-eerie rounded-lg md:mx-0 mx-3">
+					<div className="mx-auto">
+						<p className="font-bold text-white text-4xl text-center">Web UI Collection</p>
 					</div>
-					<div className="flex gap-5 opacity-80">
+					<div className="flex justify-center items-center gap-5 opacity-80">
 						<img
 							src={ReactLogo}
 							alt="react-logo"
-							className="max-h-10 max-w-10 grayscale object-contain"
+							className="max-h-10 max-w-10 object-contain grayscale"
 						/>
 						<img
 							src={TypescriptLogo}
 							alt="typescript-logo"
-							className="max-h-10 max-w-10 grayscale object-contain"
+							className="max-h-10 max-w-10 object-contain grayscale"
 						/>
 						<img
 							src={TailwindLogo}
 							alt="tailwind-logo"
-							className="max-h-10 max-w-10 grayscale object-contain"
+							className="max-h-10 max-w-10 object-contain grayscale"
 						/>
 					</div>
 					<Link
 						to="/button"
-						className="py-2 text-sm font-medium px-6 text-white bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-glow active:opacity-80 rounded flex ml-auto mr-5 md:mr-10"
+						className="py-3 font-bold text-lg w-full px-6 text-white bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-glow active:opacity-80 rounded-lg flex ml-auto justify-center items-center"
 					>
 						Explore
-						<ArrowLongRightIcon className="h-5 w-5 path-stroke-2 ml-2" />
+						<ArrowLongRightIcon className="h-7 w-7 path-stroke-3 ml-2" />
 					</Link>
 			</div>
 		</div>
